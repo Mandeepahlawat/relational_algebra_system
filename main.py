b950158edc52137bef8e1f80b3bdfb7bb6c96782
Konstantin <k_chemod@encs.concordia.ca>
2019-10-29 21:50:29 -0400

    Projection example reading file from the disk

--- main.py
+++ main.py
@@ -16,21 +16,50 @@ def parseLine(line):
         return Operation.UNION.value + " " + m.group(1) + " " + m.group(2)
     if re.search("^P\((.*)\)$", line):
         m = re.search("^P\((.*)\)$", line)
-        return Operation.PROJECTION.value + " " + m.group(1)
+        cols=m.group(1).split(",")
+        table=cols.pop(0).strip()
+        attrs=""
+        for a in cols:
+            attrs+=a.strip()+","
+        attrs=attrs[:-1]
+        return Operation.PROJECTION.value, table, attrs
     return "Error"
 
-def testQuery():
-    con = sqlite3.connect(":memory:")
+def loadTable(con, name):
     cur = con.cursor()
-    cur.execute ("CREATE TABLE flights (from_airport text, to_airport text, days text)")
-    cur.execute ("INSERT INTO flights VALUES ('YUL', 'DME', '1234567')")
-    cur.execute ("SELECT * FROM flights WHERE from_airport='YUL'")
-    print(cur.fetchone())
-    cur.close ()
+    cur.execute ("SELECT COUNT(name) FROM sqlite_master WHERE type='table' AND name='"+name+"'")
+    r=cur.fetchone()[0]
+    if (r==0):
+        with open(name+".txt") as f:
+            content=f.readlines()
+            cols=len(content[0].split("\t"))
+            create="CREATE TABLE "+name+" ("
+            names="("
+            for i in range(cols):
+                create+="c"+str(i)+" TEXT,"
+                names+="?,"
+            create=create[:-1]+")"
+            names=names[:-1]+")"
+            cur.execute (create)
+            query="INSERT INTO "+name+" VALUES "+names
+            for line in content:
+                values=tuple(line[:-1].split("\t"))
+                cur.execute(query, values)
+
+def projection(con, table, columns):
+    cur = con.cursor()
+    cur.execute ("SELECT "+columns+" FROM "+table)
+    res=cur.fetchall()
+    for line in res:
+        print(line)
+    
 
 def main():
-    inputline = "P(atr1, atr2)"
-    print(parseLine(inputline))
-    testQuery()
+    con = sqlite3.connect(":memory:")
+    inputline = "P(a, c2, c1, c0)"
+    (operation, table, attrs)=parseLine(inputline)
+    loadTable(con, table)
+    if (operation==Operation.PROJECTION.value):
+        projection(con, table, attrs)
 
 main()
