53ae1d6416cc6b331b36c6c792a15287c49b76fb
Konstantin <k_chemod@encs.concordia.ca>
2019-11-06 22:12:09 -0500

    Provenace for select, projection and union

--- provenance.py
+++ provenance.py
@@ -15,6 +15,8 @@ def get_relations(inputline):
 def get_projections(inputline):
     if re.search("project", inputline, re.IGNORECASE):
         project_cols = re.search("\<.+\>", inputline).group().replace("<", "").replace(">", "")
+        if ("provenance" not in project_cols.split(",")):
+            project_cols+=", provenance"
         return project_cols
     else:
         return '*'
@@ -24,7 +26,7 @@ def get_new(inputline):
         new_name = re.search("^.+:", inputline).group().replace(":", "")
         return new_name
     else:
-        return ''
+        return '_provenance_temp'
     
 
 def print_results(cur):
@@ -33,6 +35,16 @@ def print_results(cur):
     for result in results:
         print(result)
         print
+
+def get_columns(cur, table):
+    query='PRAGMA table_info({})'.format(table)
+    cur.execute(query)
+    results = cur.fetchall()
+    columns = []
+    for result in results:
+        if (result[1]!="provenance"):
+            columns.append(result[1])
+    return columns
 
 def process_query_provenance(inputline, cur):
     print ("Processing provenance")
@@ -51,13 +63,21 @@ def process_query_provenance(inputline, cur):
             query += ' where {}'.format(select_conditions)
         union=True
 
-    if (new_name):
-        temp='DROP TABLE IF EXISTS {}'.format(new_name)
-        cur.execute(temp)
-        query='CREATE TABLE {} AS {}'.format(new_name, query)
-        cur.execute(query)
-        query='SELECT * FROM {}'.format(new_name)
-    
-    print (query)
-    cur.execute(query)
+    temp_name="_provenance_step1"
+    temp_query='DROP TABLE IF EXISTS {}'.format(temp_name)
+    cur.execute(temp_query)
+    temp_query='CREATE TABLE {} AS {}'.format(temp_name, query)
+    print (temp_query)
+    cur.execute(temp_query)
+    columns=get_columns(cur, temp_name)
+    columns_text=",".join(columns)
+    print (columns_text)
+
+    temp_query='DROP TABLE IF EXISTS {}'.format(new_name)
+    cur.execute(temp_query)
+    temp_query='CREATE TABLE {} AS SELECT {},GROUP_CONCAT(provenance, "+") FROM {} GROUP BY {}'.format(new_name, columns_text, temp_name, columns_text)
+    print (temp_query)
+    cur.execute(temp_query)
+    temp_query='SELECT * FROM {}'.format(new_name)
+    cur.execute(temp_query)
     print_results(cur)
